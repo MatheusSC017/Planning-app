3700e3f046e226877726ddb3f1b98a1be9106368
MatheusSC017 <matheussimao2101@gmail.com>
2026-03-30 07:24:37 -0300

    Including pt-br strings with auto config in the root to select device language

--- app/src/main/java/com/matheus/planningapp/MainActivity.kt
+++ app/src/main/java/com/matheus/planningapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.matheus.planningapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,16 +9,26 @@ import androidx.compose.runtime.CompositionLocalProvider
 import com.matheus.planningapp.navigation.AppNavigation
 import com.matheus.planningapp.ui.theme.LocalStrings
 import com.matheus.planningapp.ui.theme.PlanningAppTheme
+import com.matheus.planningapp.ui.theme.StringsRepository
 import com.matheus.planningapp.ui.theme.StringsRepositoryEnglish
+import com.matheus.planningapp.ui.theme.StringsRepositoryPortuguese
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val selectedLanguage: StringsRepository = when (Locale.getDefault().language) {
+            "pt" -> StringsRepositoryPortuguese()
+            else -> StringsRepositoryEnglish()
+        }
+        Log.d("TAG", "onCreate: ${Locale.getDefault().language}")
+
         setContent {
             PlanningAppTheme {
                 CompositionLocalProvider(
-                    LocalStrings provides StringsRepositoryEnglish()
+                    LocalStrings provides selectedLanguage
                 ) {
                     AppNavigation()
                 }
--- app/src/main/java/com/matheus/planningapp/ui/theme/StringsRepository.kt
+++ app/src/main/java/com/matheus/planningapp/ui/theme/StringsRepository.kt
@@ -139,6 +139,77 @@ class StringsRepositoryEnglish: StringsRepository {
     override val hourFormat = "%02d:%02d"
 }
 
+
+class StringsRepositoryPortuguese: StringsRepository {
+    // Project information
+    override val projectName = "Planejando sua vida"
+    override val timeline = "Linha do tempo"
+
+    // Text for buttons in general
+    override val insertButton = "Inserir"
+    override val viewButton = "Visualizar"
+    override val dismissButton = "Dispensar"
+    override val confirmButton = "Confirmar"
+    override val cancelButton = "Cancelar"
+    override val deleteButton = "Excluir"
+    override val updateButton = "Atualizar"
+    override val increaseButton = "Aumentar"
+    override val decreaseButton = "Diminuir"
+
+    // Text for messages in general
+    override val savedMessage = "Salvo"
+
+    // Menu Options
+    override val menuButton = "Menu"
+    override val homeMenuButton = "Início"
+    override val calendarsMenuButton = "Calendários"
+    override val recurrencesMenuButton = "Recorrências"
+    override val settingsMenuButton = "Configurações"
+    override val backMenuButton = "Voltar"
+
+    // View Options
+    override val columnView = "Coluna"
+    override val gridView = "Grade"
+
+    // Calendar information
+    override val dialogDeleteCalendarTitle = "Excluir calendário"
+    override val dialogDeleteCalendarMessage = "Tem certeza de que deseja excluir este calendário?"
+    override val calendarNameField = "Nome do calendário"
+    override val calendarIsDefaultField = "Definir como padrão"
+
+    // Commitment information
+    override val dialogDeleteCommitmentTitle = "Excluir compromisso"
+    override val dialogDeleteCommitmentMessage = "Tem certeza de que deseja excluir este compromisso?"
+    override val commitmentTitleField = "Título"
+    override val commitmentDescriptionField = "Descrição"
+    override val commitmentStartField = "Horário de início"
+    override val commitmentEndField = "Horário de término"
+    override val commitmentPriorityField = "Prioridade"
+
+    // Recurrence information
+    override val recurrenceFrequencyField = "Frequência"
+    override val recurrenceIntervalField = "Intervalo"
+    override val recurrenceDayOfMonthField = "Dia do mês"
+    override val recurrenceWeekDaysField = "Dias da semana"
+    override val recurrenceIsRecurrenceActiveField = "Esta é uma tarefa recorrente?"
+    override val recurrenceValueField = "Valor"
+
+    // Settings information
+    override val dialogUpdateSettingTitle = "Confirmar configurações"
+    override val dialogUpdateSettingMessage = "Tem certeza de que deseja salvar as configurações?"
+    override val settingViewModeField = "Modo de visualização"
+    override val settingNotificationField = "Configuração de notificações"
+
+    // Datetime information
+    override val monthNames = listOf(
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    )
+    override val weekDaysAbbrev = listOf('D', 'S', 'T', 'Q', 'Q', 'S', 'S')
+    override val dateFormat = "%04d-%02d-%02d"
+    override val hourFormat = "%02d:%02d"
+}
+
 val LocalStrings = staticCompositionLocalOf<StringsRepository> {
     error("No Strings provided")
 }
